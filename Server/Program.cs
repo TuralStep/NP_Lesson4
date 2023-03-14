f4779904a76d22c7622d411b93fd497e57fea701
4214Lessons <112700431+4214Lessons@users.noreply.github.com>
2023-03-14 23:13:38 +0400

    Add project files.

---
+++ Server/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+var server = new UdpClient(45678);
+
+var remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+while (true)
+{
+    var bytes = server.Receive(ref remoteEP);
+    var str = Encoding.Default.GetString(bytes);
+    Console.WriteLine(str);
+}
